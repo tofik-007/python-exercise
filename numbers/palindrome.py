7e2a23d61c745ea723430b63c458f0612f564551
Taufik Sumara <83862887+tofik-007@users.noreply.github.com>
2022-10-19 17:25:58 +0530

    Add files via upload
    exercise of python as per the instructions

---
+++ numbers/palindrome.py
@@ -0,0 +1,7 @@
+#program to check if number is palindrome or not
+s = input("enter a string or number to check if it's palindrome or not ? ")
+n = s[::-1]    #reverse the string
+if s == n:
+    print(s,"is palindrome.")
+else:
+    print(s,"is not palindrome.")
